8d538ab5d9f6ca12ae199ab005ec9ae78d1f5472
Filippo Fiazza <filippo.fiazza@horsa.it>
2020-11-30 11:02:49 +0100

    OdlFaseVersamento

--- AuthSystem/Models/OdlFaseVersamento.cs
+++ AuthSystem/Models/OdlFaseVersamento.cs
@@ -1,5 +1,4 @@
-﻿using AuthSystem.Areas.Identity.Data;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
@@ -12,47 +11,21 @@ namespace AuthSystem.Models
     public class OdlFaseVersamento
     {
         [Key]
-        
-        public int IdVersamento { get; set; }
-
-        public string CodiceArticolo { get; set; }
-        [ForeignKey("CodiceArticolo ")]
-        public Articolo Articoli { get; set; }
-        public string CodiceOdl { get; set; }
-        [ForeignKey("CodiceOdl ")]
-        public Odl Odl { get; set; }
-        public int IdFaseODL { get; set; }
-        [ForeignKey("IdFaseODL ")]
-        public OdlFase OdlFasi { get; set; }
-
-        public string CodiceMacchinaFisica { get; set; }
-        [ForeignKey("CodiceMacchinaFisica ")]
-        [DisplayName("CodiceMacchinaFisica")]
-        public MacchinaFisica MacchinaFisiche { get; set; }
-        public string IdAspNetUsers { get; set; }
-        [ForeignKey("IdAspNetUsers ")]
-        [DisplayName("UserName")]
-        public ApplicationUser AspNetUsers { get; set; }
+        public string IdVersamento { get; set; }
 
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd\\-MM\\-yyyy}", ApplyFormatInEditMode = true)]
         [Required(ErrorMessage = "Immettere la data aggiornata")]
-        [DisplayName("Data inizio")]
-        public DateTime DataInizio { get; set; }
+        [DisplayName("Data")]
+        public DateTime Data { get; set; }
+        public string PezziBuoni { get; set; }
+        public string PezziDifettosi { get; set; }
 
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:dd\\-MM\\-yyyy}", ApplyFormatInEditMode = true)]
-        [Required(ErrorMessage = "Immettere la data aggiornata")]
-        [DisplayName("Data fine")]
-        public DateTime DataFine { get; set; }
+        public float TempoEffetivo { get; set; }
 
-        [DataType(DataType.Time)]
-        //[DisplayFormat(time= "{0:hh\\-mm\\-ss}", ApplyFormatInEditMode = true)]
-        [DisplayName("Tempo di lavoro netto")]
-        public DateTime TempoLavoroNetto { get; set; }
-
-        public int PezziBuoni { get; set; }
-        public int PezziScartati { get; set; }
+        public string IdFase { get; set; }
+        [ForeignKey("IdFase")]
+        public OdlFase Fasi { get; set; }
 
     }
 }
